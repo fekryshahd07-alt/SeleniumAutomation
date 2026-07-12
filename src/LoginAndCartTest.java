6a736e465fc7de7fe2a5050c83d93ed24628458a
fekryshahd07-alt <fekryshahd07@gmail.com>
2026-07-12 22:15:46 +0300

    Add Login and Add to Cart automated test

---
+++ src/LoginAndCartTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Keys;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginAndCartTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://bstackdemo.com/");
+
+        // Login
+        driver.findElement(By.id("signin")).click();
+        try { Thread.sleep(2000); } catch (Exception e) {}
+        driver.findElement(By.id("react-select-2-input")).sendKeys("demouser", Keys.ENTER);
+        driver.findElement(By.id("react-select-3-input")).sendKeys("testingisfun99", Keys.ENTER);
+        driver.findElement(By.id("login-btn")).click();
+
+        // Add to Cart
+        try { Thread.sleep(2000); } catch (Exception e) {}
+        driver.findElement(By.className("shelf-item__buy-btn")).click();
+
+        System.out.println("Test Passed: Login and Add to Cart successful!");
+
+        driver.quit();
+    }
+}
